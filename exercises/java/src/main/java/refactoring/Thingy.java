fcb0c140a854e4ae0b523eb008955c53c2b7f894
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2018-12-05 13:15:03 +0000

    Make Thingy slightly kinder

--- exercises/java/src/main/java/refactoring/Thingy.java
+++ exercises/java/src/main/java/refactoring/Thingy.java
@@ -6,33 +6,31 @@ import java.nio.charset.StandardCharsets;
 class Thingy {
 
     private int foo;
-    private int c1=0x1, d2=0b111;
-    private int foobar = new int[] {0,0,0,0,0}.length;
+    private int bar;
+    private int foobar = new int[]{0, 0, 0, 0, 0}.length;
 
     String doTheThing() {
         String s = "";
         for (; foo < Byte.MAX_VALUE - 27; foo++) s += b(foo) + " ";
-        return s.substring(0,s.length()-1);
+        return s.substring(0, s.length() - 1);
     }
 
     private String b(int foo) {
-        c1++;
-        d2--;
+        bar++;
         foobar--;
-        String s = c1 == d2||foobar==0?"":String.valueOf(foo + 1);
-        if (c1 == d2) s += reset();
-        if (foobar==0) s += f2();
+        String s = bar == 0b11 || foobar == 0 ? "" : String.valueOf(foo + 1);
+        if (bar == 0b11) s += bar();
+        if (foobar == 0) s += foo();
         return s;
     }
 
-    private String f2() {
-        foobar = new int[] {0,0,0,0,0}.length;
+    private String foo() {
+        foobar = new int[]{0, 0, 0, 0, 0}.length;
         return new String(DatatypeConverter.parseHexBinary("42757a7a"), StandardCharsets.UTF_8);
     }
 
-    private String reset() {
-        d2 = 0x1B;
-        c1 = 0x15;
+    private String bar() {
+        bar = 0;
         return new String(DatatypeConverter.parseHexBinary("46697a7a"), StandardCharsets.UTF_8);
     }
 }
