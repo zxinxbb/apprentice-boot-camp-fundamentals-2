db75853ddf69e212bfce65ae4793b71f1458db4c
Matt Edge <matt.jj.edge@gmail.com>
2021-12-09 15:48:32 +0000

    add BowlingGameTest

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace McrDigital.Bootcamp1.Checkout.Tests
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void RenameMe()
+        {
+
+        }
+    }
+}
