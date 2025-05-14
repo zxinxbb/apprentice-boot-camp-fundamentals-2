c85659be5644f6b41874fabb745ecf3808fd02ce
zainab gujar <zainab.gujar@autotrader.co.uk>
2025-05-14 11:59:06 +0100

    first test passed

---
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -0,0 +1,7 @@
+package bowling;
+
+public class Bowling {
+    public int bowl(int[] rolls) {
+        return 0;
+    }
+}
--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -2,9 +2,18 @@ package bowling;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class BowlingTest {
     @Test
-    public void renameMe() {
-        
+    public void rollsOForWholeGame_returnsScoreOf0() {
+     Bowling bowling = new Bowling();
+     int [] rolls = new int [] {0,0,0};
+
+     int score = bowling.bowl(rolls);
+
+     assertThat(score).isZero();
+
     }
 }
